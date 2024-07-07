268297067c72eef9ed23228f933f1b7d20b4dda8
Ravi Thakur <phenominal9159@gmail.com>
2024-07-07 17:55:04 +0530

    prisma version issue resolved

--- appwriteConfig.js
+++
@@ -1,9 +0,0 @@
-import { Account, Client } from "appwrite";
-const client = new Client();
-
-client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject(process.env.APPWRITE_PROJECT_KEY);
-
-export const account = new Account(client);
-export default client;
